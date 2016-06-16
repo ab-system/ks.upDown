11f713787ed0b4cdc9ede401cc33b4cd2c0cea71
egor ol't <olt.egor@gmail.com>
2016-06-16 16:45:31 +0300

    удалил не нужные зависимости

--- module.js
+++ module.js
@@ -2,7 +2,7 @@
  * Created by eolt on 08.10.2015.
  */
 
-    angular.module('components', ['ui.bootstrap', 'ngMaterial', 'ks.upDown'])
+    angular.module('components', ['ngMaterial', 'ks.upDown'])
         .controller('mainCtrl', [ '$scope', function($scope) {
             $scope.intModel = 6;
             $scope.floatModel = 2;
